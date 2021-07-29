2e13139e005a98e09fb876c0d1c0cd4ac25ba64c
eugeneswalker <eugenesunsetwalker@gmail.com>
2021-07-29 15:25:23 -0400

    update summit example to use --contain option

--- run-summit.sh
+++ run-summit.sh
@@ -15,17 +15,24 @@ AMRWIND_CMD="amr_wind $(pwd)/amr-wind.input amr.n_cell=200 200 200 geometry.prob
 
 set -x
 
-jsrun \
- --nrs 8 \
- --rs_per_host 4 \
- --tasks-per-rs 1 \
- --cpus_per_rs 1 \
- --gpus_per_rs 1 \
-   singularity run \
-     --nv \
-     --bind ${MEMBERWORK}/gen010/exawind-run:${MEMBERWORK}/gen010/exawind-run \
-     --bind ${HOME}:${HOME} \
-     --bind ${MPI_HOST}:${MPI_CONTAINER} \
-     --bind ${CUDA_HOST}:${CUDA_CONTAINER} \
-     --env LD_LIBRARY_PATH=${MPI_CONTAINER}/lib/pami_port \
-     ${SIMG} /bin/bash -c "cd ${RUNDIR} && ${AMRWIND_CMD}"
+jsrun -n8 -r4 -a1 -c1 -g1 \
+ singularity run \
+  --nv \
+  --contain \
+  --bind /dev \
+  --bind /etc/localtime \
+  --bind /etc/hosts \
+  --bind /autofs/nccs-svm1_sw \
+  --bind /ccs/sw \
+  --bind /sw \
+  --bind /autofs/nccs-svm1_proj \
+  --bind /ccs/proj \
+  --bind /sw/summitdev/singularity/98-OLCF.sh:/.singularity.d/env/98-OLCF.sh \
+  --bind /etc/libibverbs.d \
+  --bind /lib64:/host_lib64 \
+  --bind ${RUNDIR} \
+  --bind ${HOME} \
+  --bind ${MPI_HOST} \
+  --bind ${CUDA_HOST} \
+  --env LD_LIBRARY_PATH=${MPI_CONTAINER}/lib/pami_port \
+  ${SIMG} /bin/bash -c "cd ${RUNDIR} && ${AMRWIND_CMD}"
