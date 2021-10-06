d001eeab2188cd2f7f253f5fb09a7765e79f9a48
eugeneswalker <eugenesunsetwalker@gmail.com>
2021-10-06 19:19:01 -0400

    nalu-wind run script: bind mount gpfs lib dir

--- run-naluwind-summit.sh
+++ run-naluwind-summit.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo this is not ready
-exit 0
-
 _SIMG=ecpe4s-exawind-summit-2021-10-05.sif
 [[ ! -f "${_SIMG}" && -z ${SIMG+x} ]] && wget "https://cache.e4s.io/exawind/artifacts/${_SIMG}"
 SIMG=${SIMG:-$(pwd)/${_SIMG}}
@@ -32,8 +29,8 @@ NALUWIND_CMD_1="naluX -i ablNeutralNGPTrilinos.yaml"
 set -x
 
 jsrun \
- --nrs 2 \
- --rs_per_host 2 \
+ --nrs 6 \
+ --rs_per_host 6 \
  --tasks_per_rs 1 \
  --cpu_per_rs 1 \
  --gpu_per_rs 1 \
@@ -52,12 +49,15 @@ jsrun \
     --bind /sw/summitdev/singularity/98-OLCF.sh:/.singularity.d/env/98-OLCF.sh \
     --bind /etc/libibverbs.d \
     --bind /lib64:/host_lib64 \
+    --bind /usr/lpp/mmfs/lib \
     --bind ${RUNDIR} \
     --bind ${HOME} \
     --bind ${MPI_HOST} \
     --bind ${CUDA_HOST} \
-    --env LD_LIBRARY_PATH=${MPI_CONTAINER}/lib/pami_port \
+    --env LD_LIBRARY_PATH=${MPI_CONTAINER}/lib/pami_port:${MPI_CONTAINER}/lib:/usr/lpp/mmfs/lib \
     ${SIMG} /bin/bash -c "cd ${RUNDIR} && ${NALUWIND_CMD_1}"
 
-cd ${RUNDIR}
-./pass_fail.py --abs-tol 1.0e-6 --rel-tol 1.0e-7 ablNeutralNGPTrilinos ablNeutralNGPTrilinos.norm.gold
+(
+ cd ${RUNDIR}
+ ./pass_fail.py --abs-tol 1.0e-6 --rel-tol 1.0e-7 ablNeutralNGPTrilinos ablNeutralNGPTrilinos.norm.gold
+)
