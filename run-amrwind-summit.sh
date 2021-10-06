6079dd6249d16799a3b5d0297549cf5f90c88c67
eugeneswalker <eugenesunsetwalker@gmail.com>
2021-10-05 22:29:33 -0700

    include input files needed for running demos

--- run-amrwind-summit.sh
+++ run-amrwind-summit.sh
@@ -17,8 +17,8 @@ MPI_CONTAINER=${MPI_ROOT}
 CUDA_HOST=/sw/summit/cuda/11.3.1
 CUDA_CONTAINER=/sw/summit/cuda/11.3.1
 
-AMRWIND_CMD_1="amr_wind $(pwd)/amr-wind.input amr.n_cell=384 512 512 geometry.prob_hi=300.0 400.0 400.0 time.max_step=10"
-AMRWIND_CMD_1024="amr_wind ${RUNDIR}/amr-wind.input amr.n_cell=12288 8192 512 geometry.prob_hi=9600.0 6400.0 400.0 time.max_step=10"
+AMRWIND_CMD_1="amr_wind $(pwd)/inputs/amr-wind.input amr.n_cell=384 512 512 geometry.prob_hi=300.0 400.0 400.0 time.max_step=10"
+AMRWIND_CMD_1024="amr_wind $(pwd)/inputs/amr-wind.input amr.n_cell=12288 8192 512 geometry.prob_hi=9600.0 6400.0 400.0 time.max_step=10"
 
 set -x
 
